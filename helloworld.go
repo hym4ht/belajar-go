9f62d46b7f98a10a071c04ffc287bda3952f3b00
hym4ht <azrilrestum@gmail.com>
2026-01-28 23:46:27 +0700

    Initial commit: add helloworld.go

---
+++ helloworld.go
@@ -0,0 +1,8 @@
+package main //package main adalah package utama
+
+import "fmt" //import format modulww
+
+func main() {
+	fmt.Println("Hello Golang") //fmt.Println("Hello Golang") adalah perintah untuk menampilkan teks
+
+}
